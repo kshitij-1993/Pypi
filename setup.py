2eaade527e52dcb55850cba3f9d1c641b59d3c63
Kshitij <kshitijgupta1993@gmail.com>
2020-12-21 23:38:19 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
   url = 'https://github.com/kshitij-1993/Pypi.git',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/kshitij-1993/Pypi/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['KshitijProjrct'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
+  #install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
